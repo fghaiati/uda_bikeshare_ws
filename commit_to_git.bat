e2cbef41ff17ef7bce8340400292c9c870da08db
fghaiati <fathi.ghaiati@gmail.com>
2020-12-11 02:13:08 +0000

    udcity bikeshare project workspace add commit_to_git.bat

---
+++ commit_to_git.bat
@@ -0,0 +1,16 @@
+git init
+git config --global user.name "fghaiati"
+git config --global user.email "fathi.ghaiati@gmail.com"
+git add bikeshare.py
+git add chicago.csv
+git add new_york_city.csv
+git add readme.txt
+git add washington.csv
+git commit -m "udcity data analysis bikeshare project base"
+git remote add origin https://'fghaiati':'Gpass1223'@github.com/fghaiati/uda_bikeshare_ws.git
+git branch -M accepted
+git push -u origin accepted
+git add commit_to_git.bat
+git commit -m "udcity bikeshare project workspace add commit_to_git.bat"
+git branch -M accepted
+git push -u origin accepted
